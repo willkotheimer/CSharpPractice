f6c7f07b5d837a53d5236e56da7b1702c68be6e8
wkotheimer <wkotheimer@gmail.com>
2018-11-07 14:04:26 -0600

    Added Enums

---
+++ Enums/Enums/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Movie
+    {
+
+        public enum Moviegenre
+        {
+            Action,
+            Comedy,
+            SciFi,
+            Horror
+        }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public Moviegenre Genre { get; set; }
+
+        public Movie(string title, int year, Moviegenre genre)
+        {
+            Title = title;
+            Year = year;
+            Genre = genre;
+        }
+    }
+}
---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie("Star Wars", 1977, Movie.Moviegenre.SciFi);
+            printMovieGenre(movie);
+            Console.ReadKey();
+        }
+        private static void printMovieGenre(Movie movie)
+        {
+
+            switch (movie.Genre)
+            {
+
+                case Movie.Moviegenre.Action:
+                    Console.WriteLine("Action movie");
+                    break;
+                case Movie.Moviegenre.Comedy:
+                    Console.WriteLine("Comedy movie");
+                    break;
+                case Movie.Moviegenre.SciFi:
+                    Console.WriteLine("Science Fiction Movie");
+                    break;
+                case Movie.Moviegenre.Horror:
+                    Console.WriteLine("Horror Movie");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Movie Case");
+                    break;
+            
+
+        }
+        }
+    }
+
+}
