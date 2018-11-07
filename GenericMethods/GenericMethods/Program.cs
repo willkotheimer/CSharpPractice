9e6845dce1e42980f2ef192416f852bd9ffd48f2
wkotheimer <wkotheimer@gmail.com>
2018-11-07 15:20:49 -0600

    Added generic class for a stack collection

---
+++ GenericMethods/GenericMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<string> names = new MyStack<string>();
+            names.Push("Jack");
+            names.Push("Bob");
+            names.Push("Mary");
+
+            while(!names.IsEmpty())
+            {
+                string name = names.Pop();
+                Console.WriteLine(name);
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
