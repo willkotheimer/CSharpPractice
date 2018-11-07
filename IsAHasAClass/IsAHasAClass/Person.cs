e5aa2237409714d94b31443d2a7b6218b8768415
wkotheimer <wkotheimer@gmail.com>
2018-11-07 10:04:43 -0600

    Added Program and Person class to project

---
+++ IsAHasAClass/IsAHasAClass/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsAHasAClass
+{
+    class Person
+    {
+        //Basic class with getters and setters, constructor, and static variable in C#;
+
+        private readonly string _firstname;
+        private readonly string _lastname;
+
+        public static int minAge = 18;
+        public static int maxAge = 70;
+
+        public int Age { get; set; }
+
+        private decimal _salary;
+
+        public string Firstname
+        {
+            get
+            {
+                return _firstname;
+            }
+
+           
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return _lastname;
+            }
+
+           
+        }
+
+
+        public Person() { }
+
+        public Person(string firstName, string lastName, int age, decimal salary = 44000)
+        {
+            _firstname = firstName;
+            _lastname = lastName;
+            Age = age;
+            _salary = salary;
+        }
+
+    }
+}
--- IsAHasAClass/IsAHasAClass/Program.cs
+++ IsAHasAClass/IsAHasAClass/Program.cs
@@ -10,6 +10,21 @@ namespace IsAHasAClass
     {
         static void Main(string[] args)
         {
+            Person person = new Person("John","Smith",50);
+            
+            Console.WriteLine("{0} {1} - age: {2}", person.Firstname, person.Lastname, person.Age);
+           
+            if(person.Age<Person.minAge)
+            {
+                Console.WriteLine("Not old enough");
+            }else if(person.Age>Person.maxAge)
+            {
+                Console.WriteLine("Person is too old");
+            } else
+            {
+                Console.WriteLine("Age is fine!");
+            }
+            Console.ReadKey();
         }
     }
 }
