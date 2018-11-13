65fe2a218df77ac0c551f847dfbd432fc45248f3
wkotheimer <wkotheimer@gmail.com>
2018-11-13 09:12:03 -0600

    Added service and dependency injection

---
+++ RoutesMVC/RoutesMVC/Services/IEntertainmentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RoutesMVC.Models;
+
+namespace RoutesMVC.Services
+{
+    public interface IEntertainmentService
+    {
+        IEnumerable<Games> GetGames();
+        IEnumerable<Movies> GetMovies();
+    }
+}
--- RoutesMVC/RoutesMVC/Startup.cs
+++ RoutesMVC/RoutesMVC/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RoutesMVC.Services;
 
 namespace RoutesMVC
 {
@@ -24,17 +25,27 @@ namespace RoutesMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //Service instantiated every time
+            services.AddTransient<IEntertainmentService, EntertainmentServices>();
+            //Service instantiated one time and one only
+            //services.AddSingleton<IEntertainmentService, EntertainmentServices>();
+            //Service will be created everytime a request comes in
+            // services.AddScoped<IEntertainmentService, EntertainmentServices>();
+
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMvc();
         }
 
+       
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
