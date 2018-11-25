4a9be3183d336d57e67c423b00565de0b3a830a6
wkotheimer <wkotheimer@gmail.com>
2018-11-25 17:07:27 -0600

    Added validation and views

---
+++ MVCModelValidation/MVCModelValidation/Controllers/PaymentInfoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCModelValidation.Models;
+
+namespace MVCModelValidation.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    public class PaymentInfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Create");
+        }
+
+       
+        public IActionResult Create([Bind("CardNumber","ExpiresMonth","ExpiresYear","Name","Email")]PaymentInfo paymentInfo)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Success");
+            } else
+            {
+                return View("Failure");
+            }
+        }
+    }
+}
---
+++ MVCModelValidation/MVCModelValidation/Models/PaymentInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCModelValidation.Models
+{
+    public class PaymentInfo
+    {
+        [Required]
+        [StringLength(80, MinimumLength=5)]
+        public string Name { get; set; }
+
+        [Required]
+        [CreditCard]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Range(1,12)]
+        public int ExpiresMonth { get; set; }
+
+        [Required]
+        [Range(2017,2036)]
+        public int ExpiresYear { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
